b743f2d7ed070df78bdf77834549a3a40c2dc128
SYP_Job <624273987@qq.com>
2023-07-05 16:58:16 +0800

    20230705 [insert] 新增Mqtt服务（连接MQTT服务器，发布信息）

---
+++ EMS/Common/Mqtt/MqttConnectOption.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.Common.Mqtt
+{
+    public class MqttConnectOption
+    {
+        public string Address { get; set; }
+        public int Port { get; set; }
+        public string UseName { get; set; }
+        public string Password { get; set; }
+        public string ClientId { get; set; }
+    }
+}
