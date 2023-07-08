50be6e5e205f3e65f98db876c843a5a59b16c755
SYP_Job <624273987@qq.com>
2023-07-08 14:22:11 +0800

    20230708 [modify] 菜单栏中文件->退出功能

--- EMS/MainWindow.xaml.cs
+++ EMS/MainWindow.xaml.cs
@@ -1,25 +1,12 @@
 ﻿using EMS.Model;
 using EMS.Storage.DB.DBManage;
 using EMS.Storage.DB.Models;
-using EMS.View;
 using EMS.ViewModel;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Xceed.Wpf.Toolkit.PropertyGrid.Attributes;
 
 namespace EMS
 {
@@ -182,6 +169,11 @@ namespace EMS
             viewmodel.DisplayContent.IntegratedDev.BatteryTotalList.Remove(item);
             DevConnectInfoManage manage = new DevConnectInfoManage();
             manage.Delete(new DevConnectInfoModel() { IP = item.IP, Port = item.Port, BCMUID = item.BCMUID });
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
